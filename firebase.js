db9f58981fb5a906a2dc5d0d13f5063ada961e53
Vishal Singh <vishudv125@gmail.com>
2023-05-02 00:22:04 +0530

    Initial Commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA5tETlnCnoY31msfVJDDE4Ex1gXlNzV0g",
+    authDomain: "shopit-commerce.firebaseapp.com",
+    projectId: "shopit-commerce",
+    storageBucket: "shopit-commerce.appspot.com",
+    messagingSenderId: "1051193746048",
+    appId: "1:1051193746048:web:b29265b25351311172fc65",
+    measurementId: "G-CBKM8CRR4N"
+  };
