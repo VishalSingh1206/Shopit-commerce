656143c211abe7118907f991f4882736fffb9f7a
Vishal Singh <vishudv125@gmail.com>
2023-05-02 02:18:41 +0530

    Nav Bar Frontend Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-   <h1>HEY WHATSUP</h1>
+      {/* <h1>HEY WHATSUP</h1> */}
+      <Header />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,40 @@
-import React from 'react'
-import './Header.css'
-function 
-(Header) {
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import "./Header.css";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+function Header() {
   return (
-    <div className='header'>
-<img src=""></img>
+    <div className="header">
+      <img
+        className="header__logo"
+        src="https://raw.githubusercontent.com/VishalSingh1206/Shopit-commerce/master/public/Asset%203final.png"
+      ></img>
+      <div className="header__search">
+        <input className="header__searchIn" type="text"></input>
+        <SearchIcon className="header__searchIcon"></SearchIcon>
+        {/*Logo*/}
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionOne">Hello</span>
+          <span className="header__optionTwo">Sign In</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionOne">Your</span>
+          <span className="header__optionTwo">Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionOne">Get</span>
+          <span className="header__optionTwo">Prime</span>
+        </div>
+        <div className="header__optionbasket">
+          <ShoppingCartIcon />
+          <span className="header__basketcount header__optionTwo">0</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
